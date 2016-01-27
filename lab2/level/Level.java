ab3867ceaa657b8fbd1873c99a0e2ab7afc11df3
August <august.eriksson@outlook.com>
2016-01-27 23:33:47 +0100

    A bit of progress on assignment 3

--- lab2/level/Level.java
+++ lab2/level/Level.java
@@ -1,18 +1,25 @@
 
 package lab2.level;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 
 public class Level extends Observable {
-	
-	
+	private ArrayList rooms = new ArrayList();
 	public boolean place(Room r, int x, int y)  {
-		
+		if(overlaps(r)) return false;
+		rooms.add(r);
 	}
 	
 	public void firstLocation(Room r) {
 			
 	}
-	
+
+	private boolean overlaps(Room r) {
+		if (rooms.size() == 0) return false;
+		for(int i=0; i < rooms.size(); i++) {
+			if(rooms.get(i))
+		}
+	}
 }
