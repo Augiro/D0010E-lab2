c2c3691a6edaa83aee07a67fbe8bc40d8920be16
August <august.eriksson@outlook.com>
2016-01-28 13:57:47 +0100

    Added RoomLocation class

--- lab2/level/Level.java
+++ lab2/level/Level.java
@@ -7,19 +7,46 @@ import java.util.Observable;
 
 public class Level extends Observable {
 	private ArrayList rooms = new ArrayList();
+	private Room startLocation;
+
 	public boolean place(Room r, int x, int y)  {
-		if(overlaps(r)) return false;
-		rooms.add(r);
+		if(overlaps(r, x, y)) return false;
+		rooms.add(new RoomLocation(r, x, y));
 	}
 	
 	public void firstLocation(Room r) {
-			
+			startLocation = r;,
 	}
 
-	private boolean overlaps(Room r) {
+	private boolean overlaps(Room r, int x, int y) {
+		return false; // WIP
 		if (rooms.size() == 0) return false;
 		for(int i=0; i < rooms.size(); i++) {
-			if(rooms.get(i))
+			if(y + r.height )
 		}
+	}
+}
+
+class RoomLocation {
+	private Room room;
+	private int xcord;
+	private int ycord;
+
+	RoomLocation(Room room, int x, int y) {
+		this.room = room;
+		xcord = x;
+		ycord = y;
+	}
+
+	public Room room() {
+		return room;
+	}
+
+	public int xcord() {
+		return xcord;
+	}
+
+	public int ycord() {
+		return ycord;
 	}
 }
